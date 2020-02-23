eb067b8624153de2d263c883ba3d7b4e3f82bfd6
Fog314 <navimed7@gmail.com>
2020-02-23 14:51:24 +0300

    Commit 23.02.2020

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import WeatherDisplay from './WeatherDisplay.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+let PLACES = [
+    {name: 'Moscow', zip: 'Moscow'},
+]
+
+let object = JSON.parse (localStorage.getItem ('PLACES'));
+PLACES = object;
+    class App extends React.Component {
+        constructor() {
+                super();
+                this.state = {
+                activePlace: 0,
+                value: '',
+            };
+        }
+
+        onChangeHandler(event) {
+            this.setState({value: event.target.value})
+        }
+
+        handleSubmit(event) {
+            PLACES.push({name: this.state.value, zip: this.state.value});
+            localStorage.setItem('PLACES', JSON.stringify(PLACES));
+            console.log(PLACES)
+            console.log(object);
+            this.setState({value: ''})
+            event.preventDefault();
+        }
+
+        render() {
+            const activePlace = this.state.activePlace;
+            return (
+            <div className="App">
+
+                <main>
+                
+                {PLACES.map((place, index) => (
+                <button
+                    key={index}
+                    onClick={() => {
+                    this.setState({ activePlace: index });
+                    }}
+                >
+                    {place.name}
+                </button>
+                ))}
+
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                    <label>
+                        Имя:
+                        <input type="text" value={this.state.value} onChange={this.onChangeHandler.bind(this)} />
+                    </label>
+                        <input type="submit" value="Отправить" />
+                </form>
+
+                <WeatherDisplay
+                key={activePlace}
+                zip={PLACES[activePlace].zip}
+                nameInput={PLACES[PLACES.length-1].name}
+                />
+                </main>
+                <footer>
+                </footer>
+            </div>
+            );
+        }
+    }
 
 export default App;
